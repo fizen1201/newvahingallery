ca496c5e25fc5285cbd2fd61d068908ddd3d55d5
fizen1201 <62836864+fizen1201@users.noreply.github.com>
2021-05-03 08:43:32 +0530

    Add files via upload

---
+++ js/api.js
@@ -0,0 +1,4 @@
+export default{
+USER_ID: 'user_QUu4actUyJw7yXWQ4r2Bp',
+TEMPLATE_ID: 'template_1h91bg6'
+}
---
+++ js/gallery.js
@@ -0,0 +1,18 @@
+
+    // $(window).on("load", (function () {
+    //     $(".lds-ellipsis").fadeOut(), $(".preloader").delay(500).fadeOut("slow"), $("body").delay(803)
+    // }));
+
+$('[data-fancybox="gallery"]').fancybox({
+        defaults = {
+            loop: true,
+            gutter: 25,
+            buttons: [
+                "fullScreen",
+                "download",
+                "thumbs",
+                "close"
+            ],
+            idleTime: 10,
+        }
+    });
